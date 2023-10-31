1c01966dc8c92f9279d2681d5cc409178b2b57dc
Leandro Seixas Rocha <leandro.fisica@gmail.com>
2023-10-31 12:29:28 -0300

    Refactoring methods

--- QuasiGraph.py
+++ QuasiGraph.py
@@ -16,39 +16,60 @@ class QuasiGraph(Atoms):
    
     def get_coordination_numbers(self):
         distances = self.atoms.get_all_distances()
-        cn = []
-        bonded_atoms = []
+        coordination_numbers = [0] * len(self.atoms)
+        bonded_atoms = [[] for _ in range(len(self.atoms))]
+        
         for i, atom_i in enumerate(self.atoms):
-            bondedi = []
             for j, atom_j in enumerate(self.atoms):
-                if i == j:
-                    continue
-                if (distances[i,j] <= (1+self.tolerance)*(CR[atom_i.number]+CR[atom_j.number])):
-                    bondedi.append(j)
-            bonded_atoms.append(bondedi)
+                if i != j and distances[i, j] <= (1 + self.tolerance) * (CR[atom_i.number] + CR[atom_j.number]):
+                    bonded_atoms[i].append(j)
+                    coordination_numbers[i] += 1
+                    
+        return coordination_numbers, bonded_atoms
 
-        for i in bonded_atoms:
-            cn.append(len(i))
-        return cn, bonded_atoms
+## Old version
+#  def get_coordination_number(self):
+#        distances = self.atoms.get_all_distances()
+#        cn = []
+#        bonded_atoms = []
+#        for i, atom_i in enumerate(self.atoms):
+#            bondedi = []
+#            for j, atom_j in enumerate(self.atoms):
+#                if i == j:
+#                    continue
+#                if (distances[i,j] <= (1+self.tolerance)*(CR[atom_i.number]+CR[atom_j.number])):
+#                    bondedi.append(j)
+#            bonded_atoms.append(bondedi)
+#
+#        for i in bonded_atoms:
+#            cn.append(len(i))
+#        return cn, bonded_atoms
 
     def get_callevallejo_numbers(self):
-        cn, bonded_atoms = self.get_coordination_numbers()
-        cvn = []
-        for i, atom in enumerate(self.atoms):
-            bonded_i = bonded_atoms[i]
-            for j in bonded_i:
-                cvn_j = np.sum(np.take(cn, bonded_i)) / max(cn)
-            cvn.append(cvn_j)
+        coordination_numbers, bonded_atoms = self.get_coordination_numbers()
+        max_coordination_number = max(coordination_numbers, default=1)
+        cvn = [sum(coordination_numbers[j] for j in bonded_atoms[i])  / max_coordination_number for i in range(len(self.atoms))]
+
+## Old version
+#   def get_callevallejo_numbers(self):
+#        cvn = [np.sum(coordination_numbers[j] for j in bonded_atoms[i]) / max_coordination_number for i in range(len(self.atoms))]
+#        cn, bonded_atoms = self.get_coordination_numbers()
+#        cvn = []
+#        for i, atom in enumerate(self.atoms):
+#            bonded_i = bonded_atoms[i]
+#            for j in bonded_i:
+#                cvn_j = np.sum(np.take(cn, bonded_i)) / max(cn)
+#            cvn.append(cvn_j)
         return cvn
 
 
     def to_dataframe(self):
        # Atomic data
         atoms_data = [{
-            'Group': element(atom.symbol).group_id,
-            'Period': element(atom.symbol).period,
-            'Covalent radius': element(atom.symbol).covalent_radius,
-            'Electronegativity': element(atom.symbol).en_pauling,
+            'group_id': element(atom.symbol).group_id,
+            'period': element(atom.symbol).period,
+            'covalent_radius': element(atom.symbol).covalent_radius,
+            'en_pauling': element(atom.symbol).en_pauling,
         } for atom in self.atoms]
         df = pd.DataFrame(atoms_data)
 
@@ -62,12 +83,11 @@ class QuasiGraph(Atoms):
 
     def flatten(self):
         df = self.to_dataframe()
-        return df.to_numpy().flatten()
+        return df.values.flatten()
 
 
 if __name__ == '__main__':
   atoms = read(sys.argv[1])
-  gcoord = QuasiGraph(atoms).to_dataframe()
-  print(gcoord)
-#  for i in gcoord:
-#    print(i)
+  qg_atoms = QuasiGraph(atoms)
+#  print(qg_atoms.flatten())
+  print(qg_atoms.to_dataframe())
