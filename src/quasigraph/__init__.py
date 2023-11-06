209088014f38da2f5b435a36dc0d38529743cbee
Leandro Seixas Rocha <leandro.fisica@gmail.com>
2023-11-06 18:48:37 -0300

    v23.11.4

--- src/quasigraph/__init__.py
+++ src/quasigraph/__init__.py
@@ -24,6 +24,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__version__ = "23.11.3"
+__version__ = "23.11.4"
 
 from .quasigraph import QuasiGraph
