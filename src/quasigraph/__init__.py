95773f35d71b52c33933c9cd157caff9f88772f8
Leandro Seixas Rocha <leandro.fisica@gmail.com>
2024-03-08 23:44:14 -0300

    update version number

--- src/quasigraph/__init__.py
+++ src/quasigraph/__init__.py
@@ -24,7 +24,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__version__ = "24.2.9"
+__version__ = "24.3.0"
 
 from .quasigraph import QuasiGraph
 from .ptable import *
